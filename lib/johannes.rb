2a244b51ddcb3efb57c21ba8a1e0075cbd99bc21
T.J. VanSlyke <teejay.vanslyke@gmail.com>
2014-02-03 14:13:11 -0800

    Add Heroku wkhtmltoimage configuration and binary.

--- lib/johannes.rb
+++ lib/johannes.rb
@@ -1,9 +1,15 @@
 require "johannes/version"
-
 require 'johannes/base'
 require 'johannes/application'
 
 module Johannes
 end
 
-Johannes::Application.secret = ENV['JOHANNES_SECRET']
+Johannes::Application.secret = ENV['JOHANNES_SECRET'] || 'omgwtfbbq'
+
+if ENV['JOHANNES_HEROKU']
+  IMGKit.configure do |config|
+    config.wkhtmltoimage = File.dirname(__FILE__) + '/../vendor/bin/wkhtmltoimage-amd64'
+  end
+end
+
